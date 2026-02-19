e36707258ad45a459ed996f0f5d22115b309e8c0
Alessandro e Gabriele <dastdelu@gmail.com>
2026-02-19 15:41:54 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { createPageUrl } from './utils';
 import { base44 } from '@/api/base44Client';
+import { useQuery } from '@tanstack/react-query';
 import {
   LayoutDashboard,
   Building2,
